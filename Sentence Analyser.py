ea31667653ea423819e79802f04aa117a4324eff
tebogo.mojela <61325361+BL4CKDUDE@users.noreply.github.com>
2022-04-03 18:04:19 +0200

    more shit

--- Sentence Analyser.py
+++ Sentence Analyser.py
@@ -327,4 +327,4 @@ class Questions(Resource):
 api.add_resource(Questions, '/question')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=8000, debug=True)
